de5fbd8b1c93fd0fcae07e25b266161495b620ef
Akın Özgen <akinozgen17@outlook.com>
2017-01-08 17:35:47 +0300

    sunucuya bağlantı kontrolü eklendi

--- updateRepositories.js
+++ updateRepositories.js
@@ -5,10 +5,21 @@ var updateRepositories = function()
 {
 	var url = "http://projects.app/packager/server/packages/packages.json";
 	var file = fs.createWriteStream("./latest.json");
-	var request = http.get(url, function(response) {
-		response.pipe(file);
-		console.log("\nBaşarılı: ".green, "Depo Güncellendi");
+
+	http.get({ host: 'packager.app', port: 80 }, function (res) {
+		if (res.statusCode == 200)
+		{
+			var request = http.get(url, function(response) {
+				response.pipe(file);
+				console.log("\nBaşarılı: ".green, "Depo Güncellendi");
+			});
+		}
+		else
+		{
+			console.log('Hata: '.red, 'Sunucya bağlanılamadı. İnternet bağlantınızı doğrulayıp tekrar deneyin.');
+		}
 	});
+
 }
 
 module.exports = updateRepositories;
