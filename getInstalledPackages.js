f24f276bda66aa1ab08bf053628c9cbd7af5418f
Akın Özgen <akinozgen17@outlook.com>
2017-01-08 19:24:13 +0300

    hiç paket yok ise uyarı verme eklendi

--- getInstalledPackages.js
+++ getInstalledPackages.js
@@ -6,17 +6,24 @@ var getInstalledPackages = function () {
     var tableData = installed.packages;
 
     var table = new easyTable;
+    
+    if (Object.keys(tableData).length > 0)
+    {
+        Object.keys(tableData).forEach(function (key) {
+            var package = tableData[key];
+            table.cell('Program Adı'.green, package.name);
+            table.cell('Sürüm'.green, package.version);
+            table.cell('Sahibi'.green, package.provider);
+            table.cell('Website'.green, package.website);
+            table.newRow();
+        });
+        console.log(table.toString());
+    }
+    else
+    {
+        console.log('\nUyarı: '.yellow, 'Hiç kurulu paketiniz yok.');
+    }
 
-    Object.keys(tableData).forEach(function (key) {
-        var package = tableData[key];
-        table.cell('Program Adı'.green, package.name);
-        table.cell('Sürüm'.green, package.version);
-        table.cell('Sahibi'.green, package.provider);
-        table.cell('Website'.green, package.website);
-        table.newRow();
-    });
-
-    console.log(table.toString());
 }
 
 module.exports = getInstalledPackages;
