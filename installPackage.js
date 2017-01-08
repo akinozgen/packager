44dcf68654e8da02df891ada4f8ee1609da8ede0
Akın Özgen <akinozgen17@outlook.com>
2017-01-08 18:01:09 +0300

    İndirme fonksiyonu tamamandı.

--- installPackage.js
+++ installPackage.js
@@ -16,9 +16,28 @@ var installPackage = {
             console.log('Hata: '.red, 'Depo dosyanız bozuk. \'packager guncelle\' komutu ile düzeltin.');
         }
     },
-    download: function(package)
+    download: function(package, version)
     {
-        var url =
+        var state = false;
+        var url = package.versions[version];
+        var des = (process.env.TEMP + '\\' + package.name + '-' + version + '.zip');
+
+        var file = fs.createWriteStream(des);
+        var request = http.get(url, function (response) {
+            response.pipe(file);
+            console.log("Devam Ediyor: ".blue, package.name + " paketi geçici dizine indiriliyor.", des);
+            response.on('end', function () {
+                console.log('Başarılı: '.green, package.name + " paketi geçici dizine indirildi.", des);
+            });
+        });
+
+        if (fs.existsSync(des))
+            state = true;
+
+        return {
+            "path": des,
+            "state": state
+        };
     },
     install: function(code)
     {
@@ -29,7 +48,16 @@ var installPackage = {
         var package = installPackage.check(code);
         if (package)
         {
+            var download = installPackage.download(package, package.version);
             console.log('\nBaşarılı: '.green, code + ' paketi bulundu. Son sürüm: ', package.version);
+            if (download.state)
+            {
+                //kur
+            }
+            else
+            {
+                console.log('Hata: '.red, 'Dosya indirilemedi. İnternet bağlantınızı kontrol edip tekrar deneyin.');
+            }
         }
         else
         {
