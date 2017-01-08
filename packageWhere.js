2f35b8d69967e5c8d2cd12512acf3785a1ac0979
Akın Özgen <akinozgen17@outlook.com>
2017-01-08 13:09:07 +0300

    nerede alt komutu tamamlandı

--- packageWhere.js
+++ packageWhere.js
@@ -1,5 +1,18 @@
-var packageWhere = function () {
-    
+var installed = require('./init')();
+
+var packageWhere = function (code) {
+
+    var packages = installed.packages;
+    if (packages[code])
+    {
+        var requested = packages[code];
+        console.log(requested.name + " paketinin kurulu olduğu dizin: ", requested.installation_path);
+    }
+    else
+    {
+        console.log("Uyarı".yellow, "Belirtilen paket zaten kurulu değil.");
+    }
+
 }
 
 module.exports = packageWhere;
