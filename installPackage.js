499686f5d9645121f875bc90e2c4ca2eeb667c19
Akın Özgen <akinozgen17@outlook.com>
2017-01-08 14:25:12 +0300

    check ve batch fonksiyonları oluşturuldu

--- installPackage.js
+++ installPackage.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const jsonFile = require('jsonfile');
+const updateRepositories = require('./updateRepositories');
 
 var installPackage = {
     check: function (code)
     {
-
+        var latest = jsonFile.readFile('./latest.json', function(err) {
+            if (String(err).indexOf('Unexpected end of JSON input') > -1)
+                console.log('Hata: '.red, "Yerel depo dosyası bozuk. 'packager guncelle' komutu ile düzeltin.");
+        });
     },
     download: function(code)
     {
@@ -16,7 +20,11 @@ var installPackage = {
     },
     batch: function(code)
     {
-        
+        var checking = installPackage.check(code);
+        if (checking)
+        {
+            console.log('\nBaşarılı: '.green, code + ' paketi bulundu. Son sürüm: ', checking);
+        }
     }
 };
 
