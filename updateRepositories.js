d89e670e99329b6aa53e5d9d45b5e31a4ae10682
Akın Özgen <akinozgen17@outlook.com>
2017-01-08 14:24:33 +0300

    debug sonrası değişiklikler

--- updateRepositories.js
+++ updateRepositories.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 const http = require('http');
 
-
 var updateRepositories = function()
 {
 	var url = "http://projects.app/packager/server/packages/packages.json";
-
 	var file = fs.createWriteStream("./latest.json");
 	var request = http.get(url, function(response) {
 		response.pipe(file);
