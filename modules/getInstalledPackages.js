619ce731f830b5a9de8c114b4330ae79858becee
Akın Özgen <akinozgen17@outlook.com>
2017-01-18 00:11:34 +0300

    listele fonksiyonunun güncellemeye uygun değişiklikleri yapıldı.

--- modules/getInstalledPackages.js
+++ modules/getInstalledPackages.js
@@ -1,29 +1,30 @@
-const easyTable = require('easy-table');
-const colors = require('colors');
-var installed = require('./init')();
+const colors = require('colors')
+const jsonFile = require('jsonfile')
+const easyTable = require('easy-table')
 
 var getInstalledPackages = function () {
-    var tableData = installed.packages;
+    var installed = jsonFile.readFileSync(process.env.PROGRAMS + '\\installed.json')
+    var tableData = installed.packages
 
-    var table = new easyTable;
-    
+    var table = new easyTable
+
     if (Object.keys(tableData).length > 0)
     {
         Object.keys(tableData).forEach(function (key) {
-            var package = tableData[key];
-            table.cell('Program Adı'.green, package.name);
-            table.cell('Sürüm'.green, package.version);
-            table.cell('Sahibi'.green, package.provider);
-            table.cell('Website'.green, package.website);
-            table.newRow();
+            var package = tableData[key]
+            table.cell('Program Adı'.green, package.name)
+            table.cell('Sürüm'.green, package.version)
+            table.cell('Sahibi'.green, package.provider)
+            table.cell('Website'.green, package.website)
+            table.newRow()
         });
-        console.log(table.toString());
+        console.log(table.toString())
     }
     else
     {
-        console.log('\nUyarı: '.yellow, 'Hiç kurulu paketiniz yok.');
+        console.log('\nUyarı: '.yellow, 'Hiç kurulu paketiniz yok.')
     }
 
 }
 
-module.exports = getInstalledPackages;
+module.exports = getInstalledPackages
